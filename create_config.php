539059b87ab6e1208deaf887caf12379daac94d0
Halfsaltedfish <379369845@qq.com>
2019-05-19 10:21:12 +0800

    v1

---
+++ create_config.php
@@ -0,0 +1,34 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+?>
+<?php
+$config = file_get_contents('php://input');
+
+// check for required fields
+if (strlen($config) > 50) {
+  $result = file_put_contents("config.txt",$config);
+    // check result
+  if ($result>50) {
+        // successfully put into file
+    $response["success"] = 1;
+    $response["message"] = "Config successfully created.";
+
+        // echoing JSON response
+    echo json_encode($response);
+  } else {
+        // failed to insert row
+    $response["success"] = 0;
+    $response["message"] = "put failed";
+
+        // echoing JSON response
+    echo json_encode($response);
+  }
+} else {
+    // required field is missing
+  $response["success"] = 0;
+  $response["message"] = "Required field(s) is missing";
+
+    // echoing JSON response
+  echo json_encode($response);
+}
+?>
---
+++ get_config.php
@@ -0,0 +1,4 @@
+<?php
+// get config from file
+echo file_get_contents("config.txt")
+?>
