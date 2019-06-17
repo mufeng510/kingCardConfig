39c66d3d69664e261bfdae7397e923be1a36faf9
Halfsaltedfish <379369845@qq.com>
2019-06-17 17:34:59 +0800

    Update autoGet.php

--- autoGet.php
+++ autoGet.php
@@ -7,9 +7,8 @@ $interval=60*10;// 每隔10分钟运行
 
 do{
 
-  file_get_contents("create.php?id=4")
-
-  sleep($interval);// 等待
+	file_get_contents("create.php?id=4")
+	sleep($interval);// 等待
 
 }while(true);
 ?>
